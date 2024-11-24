e3d893c9082a8663ff803a2d4b8304f99b04cf8b
Samuel <60676867+sjhaleprogrammer@users.noreply.github.com>
2024-11-23 23:09:47 -0500

    Update podman.nix

--- modules/nixpkgs/podman.nix
+++ modules/nixpkgs/podman.nix
@@ -1,12 +1,17 @@
-#podman
-  virtualisation = {
-    podman = {
-      enable = true;
+{ pkgs, ... }:
 
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
+{
 
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
+    #podman
+    virtualisation = {
+      podman = {
+        enable = true;
+  
+        # Create a `docker` alias for podman, to use it as a drop-in replacement
+        dockerCompat = true;
+  
+        # Required for containers under podman-compose to be able to talk to each other.
+        defaultNetwork.settings.dns_enabled = true;
+      };
     };
-  };
+}
