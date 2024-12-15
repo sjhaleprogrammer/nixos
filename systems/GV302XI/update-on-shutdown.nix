2b12f81fd74ca14c41c808663101ad41f732d9e9
samuel <samworlds1231337@gmail.com>
2024-12-15 15:13:32 -0500

    autoupgrades

--- systems/GV302XI/update-on-shutdown.nix
+++ systems/GV302XI/update-on-shutdown.nix
@@ -8,6 +8,11 @@
     enable = true;
     operation = "boot";
     flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
     dates = "daily";
     persistent = true;
   };
