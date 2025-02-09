d888b26481ea6dc56591a25541ecd7c51e95a76d
samuel <samworlds1231337@gmail.com>
2025-02-09 16:10:55 -0500

    .

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -5,6 +5,7 @@
   
   programs.zsh = {
       enable = true;
+      dotDir = ".";
       enableCompletion = true;
       autosuggestions.enable = true;
       syntaxHighlighting.enable = true;
--- nixpkgs/qemu.nix
+++ nixpkgs/qemu.nix
@@ -9,17 +9,17 @@
    virtualisation.libvirtd = {
       enable = true;
       qemu = {
-	package = pkgs.qemu_kvm;
-	runAsRoot = true;
-	swtpm.enable = true;
-	ovmf = {
-	  enable = true;
-	  packages = [(pkgs.OVMF.override {
-	    secureBoot = true;
-	    tpmSupport = true;
-	  }).fd];
-	};
-      };
+		package = pkgs.qemu_kvm;
+		runAsRoot = true;
+		swtpm.enable = true;
+		ovmf = {
+			enable = true;
+			packages = [(pkgs.OVMF.override {
+				secureBoot = true;
+				tpmSupport = true;
+			}).fd];
+	    };
+	  };
     }; 
 
 
