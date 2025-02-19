dc8cf6f6d4e347f60e0a752f1c3edc383f447350
samuel <samworlds1231337@gmail.com>
2025-02-19 14:52:28 -0500

    .

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
             inherit inputs system user shell;
           };
           modules = [
-            {nix.settings.trusted-users = [ "root" ${user} ];}
+            {nix.settings.trusted-users = [ "root" user ];}
 
             #system
             ./systems/GV302XI/configuration.nix
