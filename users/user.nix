f7e683b667325333f3ef9d56c16aaafaf853ccd0
samuel <samworlds1231337@gmail.com>
2025-02-19 12:28:35 -0500

    .

--- users/user.nix
+++ users/user.nix
@@ -44,10 +44,7 @@
     settings.auto-optimise-store = true;
     settings.trusted-users = [ "root" user ];
 
-    extraOptions = ''
-         extra-substituters = https://devenv.cachix.org
-         extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
-    '';
+    
 
   };
 
